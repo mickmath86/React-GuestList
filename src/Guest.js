6b54628e7e29baeaec7baa974b6ce6986404a9c2
Michael Mathias <mickmath86@gmail.com>
2018-05-30 21:17:53 -0700

    v.1 GuestList Commit

---
+++ src/Guest.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import GuestName from './GuestName';
+
+
+const Guest = props =>
+  <li>
+    <GuestName
+      isEditing={props.isEditing}
+      handleNameEdits={e => props.setName(e.target.value)}>
+      {props.name}
+    </GuestName>
+    <label>
+      <input
+        type="checkbox"
+        checked={props.isConfirmed}
+        onChange={props.handleConfirmation}
+        /> Confirmed
+    </label>
+    <button onClick={props.handleToggleEditing}>{props.isEditing ? "Save" : "Edit"}</button>
+    <button onClick={props.handleRemove}>remove</button>
+  </li>;
+
+
+Guest.propTypes = {
+  name: PropTypes.string.isRequired,
+  isConfirmed: PropTypes.bool.isRequired,
+  handleToggleEditing: PropTypes.func.isRequired,
+  isEditing: PropTypes.bool.isRequired,
+  setName: PropTypes.func.isRequired,
+  handleRemove: PropTypes.func.isRequired,
+
+}
+
+export default Guest;
